9d587d3ef8c90a15d59480466dc189a75a63cac9
carraes <carraeshb@gmail.com>
2023-07-29 18:15:00 -0300

    feat: create image endpoint

---
+++ src/handlers/images.tsx
@@ -0,0 +1,37 @@
+import { setup } from '@/db/setup'
+import axios from 'axios'
+import { Elysia, t } from 'elysia'
+
+export const apiRoutes = (app: Elysia) =>
+  app.use(setup).group('/images', (app) =>
+    app
+      .post(
+        '/create',
+        async ({ body: { content } }) => {
+          console.log(content)
+          const response = await axios.post(
+            'https://api.runpod.ai/v2/sd-anything-v4/run',
+            {
+              method: 'POST',
+              input: {
+                prompt: content,
+                negative_prompt:
+                  'blurry, side looking, duplication, lowres, cropped, worst quality, low quality, jpeg artifacts, out of frame, watermark, signature',
+              },
+            },
+            {
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: process.env.RUNPOD_API_KEY,
+              },
+            },
+          )
+
+          console.log(response.data)
+        },
+        { body: t.Object({ content: t.String({ minLength: 1 }) }) },
+      )
+      .post('/webhook', (ctx) => {
+        console.log(ctx.request)
+      }),
+  )
