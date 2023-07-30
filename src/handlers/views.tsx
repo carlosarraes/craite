f1cd391c5ecbefd2f36ea070dda8513068d6375d
carraes <carraeshb@gmail.com>
2023-07-30 17:13:05 -0300

    feat: add component that refreshes images

--- src/handlers/views.tsx
+++ src/handlers/views.tsx
@@ -1,5 +1,25 @@
 import elements from 'typed-html'
 import { Elysia } from 'elysia'
 import { Dashboard } from '@/view/dashboard'
+import { setup } from '@/db/setup'
+import { images } from '@/db/schema'
+import { desc, isNotNull } from 'drizzle-orm'
 
-export const viewRoutes = (app: Elysia) => app.get('/dashboard', () => <Dashboard />)
+export const viewRoutes = (app: Elysia) =>
+  app
+    .use(setup)
+    .get('/dashboard', () => <Dashboard />)
+    .get('/show', async ({ store: { db } }) => {
+      const image = await db
+        .select()
+        .from(images)
+        .where(isNotNull(images.url))
+        .orderBy(desc(images.id))
+        .get()
+
+      return (
+        <div hx-get="/show" hx-swap="outerHTML" hx-trigger="load delay:10s" class="rounded shadow">
+          <img src={image.url!} alt={image.content} class="w-96 h-96 rounded" />
+        </div>
+      )
+    })
