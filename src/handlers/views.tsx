cd23ec4d94db6d6bc355e1aa95108a701e3d64f4
carraes <carraeshb@gmail.com>
2023-07-30 18:15:35 -0300

    feat: endpoint to give user their generated image

--- src/handlers/views.tsx
+++ src/handlers/views.tsx
@@ -1,9 +1,9 @@
 import elements from 'typed-html'
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { Dashboard } from '@/view/dashboard'
 import { setup } from '@/db/setup'
 import { images } from '@/db/schema'
-import { desc, isNotNull } from 'drizzle-orm'
+import { desc, eq, isNotNull } from 'drizzle-orm'
 
 export const viewRoutes = (app: Elysia) =>
   app
@@ -23,3 +23,49 @@ export const viewRoutes = (app: Elysia) =>
         </div>
       )
     })
+    .get(
+      '/show/:id',
+      async ({ store: { db }, params: { id } }) => {
+        const image = await db.select().from(images).where(eq(images.imageId, id)).get()
+
+        if (!image.url) {
+          return (
+            <div
+              id="image-id"
+              class="flex justify-between items-center w-full text-xs mt-2 pl-1"
+              hx-get={`/show/${id}`}
+              hx-trigger="every 4s"
+              hx-swap="outerHTML"
+            >
+              <span class="text-xs">{id}</span>
+              <div class="flex gap-2 items-center">
+                <span class="text-sky-600 animate-pulse">Creating...</span>
+                <div class="w-2 h-2 bg-yellow-600 opacity-20 rounded"></div>
+                <div class="w-2 h-2 bg-sky-600 animate-pulse rounded"></div>
+                <div class="w-2 h-2 bg-green-600 opacity-20 rounded"></div>
+              </div>
+            </div>
+          )
+        }
+
+        return (
+          <div id="image-id" class="flex justify-between items-center w-full text-xs mt-2 pl-1">
+            <span class="text-xs">{id}</span>
+            <div class="flex gap-2 items-center">
+              <div>
+                <span class="text-green-600">Done</span>
+                <a href={image.url!} target="_blank">
+                  <span class="text-green-600 underline font-bold">Link</span>
+                </a>
+              </div>
+              <div class="w-2 h-2 bg-yellow-600 opacity-20 rounded"></div>
+              <div class="w-2 h-2 bg-sky-600 opacity-20 rounded"></div>
+              <div class="w-2 h-2 bg-green-600 rounded"></div>
+            </div>
+          </div>
+        )
+      },
+      {
+        params: t.Object({ id: t.String({ minLength: 1 }) }),
+      },
+    )
