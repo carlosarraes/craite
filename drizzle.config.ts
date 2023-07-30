2a0c8a532be116426f741854cddbc286e976081f
carraes <carraeshb@gmail.com>
2023-07-30 17:09:54 -0300

    chore: add drizzle configs, changes to schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './src/db/schema.ts',
   driver: 'turso',
+  out: './drizzle',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
     authToken: process.env.DATABASE_AUTH_TOKEN,
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,11 +3,12 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const images = sqliteTable('images', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  seed: integer('seed').notNull(),
   imageId: text('image_id').notNull(),
   content: text('content').notNull(),
   url: text('url'),
-  createdAt: integer('created_at', { mode: 'timestamp' }),
-  timestamp: text('timestamp').default(sql`CURRENT_TIMESTAMP`),
+  createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`),
+  expiresAt: text('expires_at'),
 })
 
 export type Image = InferModel<typeof images>
